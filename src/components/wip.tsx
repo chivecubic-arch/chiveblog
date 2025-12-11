fe04fe2b673e681124b3ef671946cd7dd35fc898
Chive <chivecubic@gmail.com>
2025-12-11 21:00:45 +0800

    Update wip.tsx

--- src/components/wip.tsx
+++ src/components/wip.tsx
@@ -13,7 +13,7 @@ export default function WIP() {
 					transition={{ delay: INIT_DELAY }}
 					className='card relative flex flex-col items-center gap-6 p-12 text-center'>
 					<div className='text-6xl'>🚧</div>
-					<h1 className='text-3xl font-bold'>开发中</h1>
+					<h1 className='text-3xl font-bold'>Fighting</h1>
 					<p className='text-secondary text-lg leading-relaxed'>这个功能正在努力开发中，敬请期待！</p>
 					<div className='mt-4 flex gap-2'>
 						<div className='h-2 w-2 animate-bounce rounded-full bg-black/20' style={{ animationDelay: '0ms' }}></div>
